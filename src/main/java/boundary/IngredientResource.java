ca51b7e6ee9faa780fea16fd2a481113b1bbdbaa
xchopin <xchopin54@gmail.com>
2017-01-15 16:09:38 +0100

    Added findAll() function

--- src/main/java/boundary/IngredientResource.java
+++ src/main/java/boundary/IngredientResource.java
@@ -3,8 +3,11 @@ package boundary;
 import entity.Ingredient;
 
 import javax.ejb.Stateless;
+import javax.persistence.CacheStoreMode;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
 
 @Stateless
 public class IngredientResource {
@@ -15,10 +18,20 @@ public class IngredientResource {
     /**
      * Method that returns an ingredient for an id given
      * @param id
-     * @return Ingredient of this id
+     * @return Ingredient
      */
     public Ingredient findById(String id) {
         return entityManager.find(Ingredient.class, id);
+    }
+
+    /**
+     * Method that returns all the ingredients
+     * @return List of Ingredient
+     */
+    public List<Ingredient> findAll(){
+        Query query = entityManager.createNamedQuery("Ingredient.findAll", Ingredient.class);
+        query.setHint("javax.persistence.cache.storeMode", CacheStoreMode.REFRESH); // Solve cache issues
+        return query.getResultList();
     }
 
 }
