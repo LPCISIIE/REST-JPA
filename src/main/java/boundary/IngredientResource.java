fdc8af93ae8e2d7787be4dc97be2559593f7c0bf
xchopin <xchopin54@gmail.com>
2017-01-15 19:39:07 +0100

    Created fake inserts into the database

--- src/main/java/boundary/IngredientResource.java
+++ src/main/java/boundary/IngredientResource.java
@@ -7,7 +7,9 @@ import javax.persistence.CacheStoreMode;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Stateless
 public class IngredientResource {
@@ -32,6 +34,36 @@ public class IngredientResource {
         Query query = entityManager.createNamedQuery("Ingredient.findAll", Ingredient.class);
         query.setHint("javax.persistence.cache.storeMode", CacheStoreMode.REFRESH); // Solve cache issues
         return query.getResultList();
+    }
+
+    /**
+     * Method that insert an ingredient into the database
+     * @param ingredient to add
+     * @return the ingredient added
+     */
+    public Ingredient insert(Ingredient ingredient) {
+        ingredient.setId(UUID.randomUUID().toString());
+        return this.entityManager.merge(ingredient);
+    }
+
+    /**
+     * Method that creates fake insert into the database
+     */
+    public void feedCatalog(){
+        this.insert(new Ingredient("Laitue",1.00));
+        this.insert(new Ingredient("Roquette",1.00));
+        this.insert(new Ingredient("Mache",1.00));
+        this.insert(new Ingredient("Carotte",1.50));
+        this.insert(new Ingredient("Concombre",1.50));
+        this.insert(new Ingredient("Tomate",1.50));
+        this.insert(new Ingredient("Jambon",2.50));
+        this.insert(new Ingredient("Jambon cru",2.50));
+        this.insert(new Ingredient("Burger",3.00));
+        this.insert(new Ingredient("Confit",3.00));
+        this.insert(new Ingredient("Emmental",1.50));
+        this.insert(new Ingredient("Comté",1.50));
+        this.insert(new Ingredient("Vinaigrette",0.50));
+        this.insert(new Ingredient("Moutarde",0.50));
     }
 
 }
