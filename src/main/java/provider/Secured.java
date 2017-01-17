2163494845a24ab01612dc5b49fa227d3683b1f6
Xavier Chopin <xchopin54@gmail.com>
2017-01-17 23:22:58 +0100

    Secured the REST endpoints

---
+++ src/main/java/provider/Secured.java
@@ -0,0 +1,12 @@
+package provider;
+
+import javax.ws.rs.NameBinding;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Target;
+
+@NameBinding
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RUNTIME)
+public @interface Secured {}
