c2528638fe72a73d8dc2d04e0fd5140645337363
Xavier Chopin <xchopin54@gmail.com>
2017-01-17 23:43:02 +0100

    Added filter for tokens

---
+++ src/main/java/provider/AuthenticationFilter.java
@@ -0,0 +1,37 @@
+package provider;
+
+import control.KeyGenerator;
+import io.jsonwebtoken.Jwts;
+
+import javax.annotation.Priority;
+import javax.ws.rs.NotAuthorizedException;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.security.Key;
+
+
+@Secured
+@Provider
+@Priority(Priorities.AUTHENTICATION)
+public class AuthenticationFilter implements ContainerRequestFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        String authHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+        if ( authHeader == null || !authHeader.startsWith("Bearer "))
+            throw new NotAuthorizedException("Not Authorized !");
+        String token = authHeader.substring("Bearer".length()).trim();
+        try {
+            Key key = new KeyGenerator().generateKey();
+            Jwts.parser().setSigningKey(key).parseClaimsJws(token);
+        } catch (Exception e) {
+            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
+        }
+    }
+
+}
