c36d7b1be99bf4b07c098dc4497efd44c7a25871
Xavier Chopin <xchopin54@gmail.com>
2017-01-19 18:51:25 +0100

    Refactored

--- src/main/java/boundary/Sandwich/SandwichRepresentation.java
+++ src/main/java/boundary/Sandwich/SandwichRepresentation.java
@@ -40,91 +40,19 @@ public class SandwichRepresentation {
     @Path("/add") 
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     public Response add (
-            @FormParam("salade") String salades,
-            @FormParam("crudite") String crudites,
+            @FormParam("salade") String salade,
+            @FormParam("crudite") String crudite,
             @FormParam("charcuterie") String charcuterie,
             @FormParam("fromage") String fromage,
             @FormParam("sauce") String sauce,
             @FormParam("viande") String viande
     ) {
-        if (sandwichResource.insert(new Sandwich(salades,crudites,charcuterie,fromage,sauce,viande)) == null)
+
+        if (sandwichResource.insert(salade,crudite,charcuterie,fromage,sauce,viande) == null)
             return Response.status(Response.Status.EXPECTATION_FAILED).build();
 
        return Response.ok().build();
     }
-   
-
-//    @POST
-//    @Path("/id/{sandwichId}")
-//    public Response editSandwich(
-//            @PathParam("sandwichId") String sandwichId,
-//            @FormParam("name") String name,
-//            @FormParam("categoryId") String categoryId,
-//            @FormParam("price") double price,
-//            @FormParam("description") String description
-//    ) {
-//        Sandwich sandwich = sandwichResource.findById(sandwichId);
-//
-//        boolean isFormEmpty = (name == null && categoryId == null && Double.toString(price) == null && description == null);
-//
-//        if (sandwich == null || isFormEmpty)
-//            return Response.notModified().build();
-//
-//        String c,n,d;
-//        double p;
-//
-//        n = (name == null) ? sandwich.getName() : name ;
-//        if (categoryId == null)
-//            c = sandwich.getCategory() ;
-//        else {
-//            Category cat = categoryResource.findById(categoryId);
-//            if (cat == null)
-//                return Response.notModified().build();
-//            else
-//                c = cat.getId();
-//        }
-//
-//        d = (description == null) ? sandwich.getDescription() : description ;
-//        p = (Double.toString(price) == null) ? sandwich.getPrice() : price;
-//
-//
-//        if (sandwichResource.update(sandwich.update(c,n,p,d)) == null)
-//            return Response.notModified().build();
-//
-//
-//        return Response.ok().build();
-//    }
-
-//    @DELETE
-//    @Path("/id/{sandwichId}")
-//    public Response deleteSandwich(@PathParam("sandwichId") String sandwichId) {
-//        if (sandwichResource.delete(sandwichId))
-//            return Response.ok().build();
-//        else
-//            return Response.status(Response.Status.NO_CONTENT).build();
-//    }
-
-
-//
-//    @PUT
-//    @Path("/bread/add")
-//    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-//    public Response addBread (
-//            @FormParam("name") String name,
-//            @FormParam("price") double price,
-//            @FormParam("description") String description
-//    ) {
-//
-//        if (name == null || Double.toString(price) == null || description == null)
-//            return Response.status(Response.Status.EXPECTATION_FAILED).build();
-//
-//        List<Category> query = categoryResource.findByName("Pain");
-//        Category breadCategory = (query.size() > 0) ? query.get(0) : categoryResource.insert(new Category("Pain"));
-//        sandwichResource.insert(new Sandwich(breadCategory,name,price,description));
-//
-//        return Response.ok().build();
-//    }
-//
 
 
 }
