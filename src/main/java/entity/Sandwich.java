6a91efb79b3831812c1814b9bf20e282992d07d0
Xavier Chopin <xchopin54@gmail.com>
2017-01-21 18:18:54 +0100

    + : price,size, price function / Improved validator

--- src/main/java/entity/Sandwich.java
+++ src/main/java/entity/Sandwich.java
@@ -16,17 +16,26 @@ import java.util.List;
 public class Sandwich implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    private final static String SANDWICH_CUSTOM = "CUSTOM";
+    private final static String SANDWICH_SIZE_UNDEFINED = "UNDEFINED";
+    private final static String SANDWICH_SIZE_0 = "PETIT";
+    private final static String SANDWICH_SIZE_1 = "MOYEN";
+    private final static String SANDWICH_SIZE_2 = "GRAND";
+    private final static String SANDWICH_SIZE_3 = "OGRE";
 
     @Id
     private String id;
 
     private String name;
 
-    private final static String SANDWICH_CUSTOM = "CUSTOM";
+    private String size;
+
+    private double price;
 
     @ManyToMany
     @JsonManagedReference
     List<Ingredient> ingredientsList;
+
     @XmlElement(name="_links")
     @Transient
     private List<Link> links = new ArrayList<>();
@@ -35,7 +44,9 @@ public class Sandwich implements Serializable {
      * Empty constructor
      */
     public Sandwich() {
+        size = SANDWICH_SIZE_UNDEFINED;
         name = SANDWICH_CUSTOM;
+        price = 0;
         ingredientsList = new ArrayList<>();
     }
 
@@ -43,8 +54,10 @@ public class Sandwich implements Serializable {
      * Constructor of a sandwich
      * @param ingredients the ingredients in a sandwich
      */
-    public Sandwich(Ingredient ... ingredients) {
+    public Sandwich(String size, Ingredient ... ingredients) {
+        price = 0;
         name = SANDWICH_CUSTOM;
+        this.size = size;
         ingredientsList = new ArrayList<>();
         for (Ingredient ingredient : ingredients)
             ingredientsList.add(ingredient);
@@ -56,8 +69,10 @@ public class Sandwich implements Serializable {
      * @param name the sandwich name
      * @param ingredients the ingredients in a sandwich
      */
-    public Sandwich(String name, Ingredient ... ingredients) {
+    public Sandwich(String size, String name, Ingredient ... ingredients) {
+        price = 0;
         this.name = name;
+        this.size = size;
         ingredientsList = new ArrayList<>();
         for (Ingredient ingredient : ingredients)
             ingredientsList.add(ingredient);
@@ -100,10 +115,9 @@ public class Sandwich implements Serializable {
     public List<Ingredient> getIngredient(String category) {
         if (!ingredientsList.isEmpty()) {
             List<Ingredient> res = new ArrayList<>();
-            for (Ingredient ingredient : ingredientsList) {
+            for (Ingredient ingredient : ingredientsList)
                 if (ingredient.categoryName().equals(category))
-                    ingredientsList.add(ingredient);
-            }
+                    res.add(ingredient);
 
             if (!res.isEmpty())
                 return res;
@@ -111,21 +125,100 @@ public class Sandwich implements Serializable {
         return null;
     }
 
+    /**
+     * Helper function that validates the composition of a sandwich
+     * @return if it's valid
+     */
     public boolean validate() {
-        boolean salad = getIngredient("Salade").isEmpty();
-        boolean meat = getIngredient("Viande").isEmpty();
-        boolean coldMeat = getIngredient("Charcuterie").isEmpty();
-        boolean cheese = getIngredient("Fromage").isEmpty();
-        boolean sauce = getIngredient("Sauce").isEmpty();
+        boolean salad =    getIngredient("Salade") != null;
+        boolean meat =     getIngredient("Viande") != null;
+        boolean coldMeat = getIngredient("Charcuterie") != null;
+        boolean cheese =   getIngredient("Fromage") != null;
+        boolean sauce =    getIngredient("Sauce") != null;
+        boolean crudite = getIngredient("Crudité") != null;
+        boolean bread =    getIngredient("Pain") != null;
 
-        if ( salad && meat && coldMeat && cheese && sauce )
-            return true;
+        if ( salad && meat && coldMeat && cheese && sauce && crudite && bread) {
+            switch(this.size) {
+                case (SANDWICH_SIZE_0) :
+                    if (ingredientsList.size() == 7)
+                        return true;
+                    break;
+                case (SANDWICH_SIZE_1) :
+                    if (ingredientsList.size() == 8)
+                        return true;
+                    break;
+                case (SANDWICH_SIZE_2) :
+                    if (ingredientsList.size() == 9)
+                        return true;
+                    break;
+                case (SANDWICH_SIZE_3) :
+                    if (ingredientsList.size() == 10)
+                        return true;
+                    break;
+            }
+        }
         return false;
+    }
+
+    /**
+     * Method that give the price of a sandwich
+     */
+    public void calculatePrice() {
+        double price = 0;
+
+        switch (size) {
+            case (SANDWICH_SIZE_1) :
+                price += 1.0;
+                break;
+            case (SANDWICH_SIZE_2) :
+                price += 1.5;
+                break;
+            case (SANDWICH_SIZE_3) :
+                price += 2.0;
+                break;
+        }
+
+        for (Ingredient ingredient : ingredientsList)
+            price += ingredient.getPrice();
+        this.price = price;
     }
 
     /**
      * - Getter and Setter functions -
      */
+
+    public static String getSandwichSize0() {
+        return SANDWICH_SIZE_0;
+    }
+
+    public static String getSandwichSize1() {
+        return SANDWICH_SIZE_1;
+    }
+
+    public static String getSandwichSize2() {
+        return SANDWICH_SIZE_2;
+    }
+
+    public static String getSandwichSize3() {
+        return SANDWICH_SIZE_3;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
     public List<Link> getLinks() {
         return links;
