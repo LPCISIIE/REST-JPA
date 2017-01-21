ff2a68f38a0bc5f257b675e7b8dc9f0a9398f61b
Xavier Chopin <xchopin54@gmail.com>
2017-01-21 15:06:56 +0100

    Fixed Error + Added URI methods

--- src/main/java/boundary/Sandwich/SandwichRepresentation.java
+++ src/main/java/boundary/Sandwich/SandwichRepresentation.java
@@ -1,13 +1,14 @@
 package boundary.Sandwich;
 
+import boundary.Ingredient.IngredientRepresentation;
+import boundary.Ingredient.IngredientResource;
+import entity.Ingredient;
 import entity.Sandwich;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.*;
-import javax.ws.rs.core.GenericEntity;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.*;
 import java.util.List;
 
 @Path("/sandwiches")
@@ -19,40 +20,95 @@ public class SandwichRepresentation {
     @EJB
     SandwichResource sandwichResource;
 
+   
+
     @GET
-    public Response getSandwichs() {
-        GenericEntity<List<Sandwich>> list = new GenericEntity<List<Sandwich>>(sandwichResource.findAll()){};
-        return Response.ok(list, MediaType.APPLICATION_JSON).build();
+    public Response getSandwichs(@Context UriInfo uriInfo) {
+        List<Sandwich> list = sandwichResource.findAll();
+
+        list.stream().forEach(sandwich -> {
+            List<Ingredient> ingredientsList = sandwich.getIngredientsList();
+            sandwich.addLink(this.getUriForSelfSandwich(uriInfo,sandwich),"self");
+            for (Ingredient ingredient : ingredientsList) {
+                ingredient.getLinks().clear();
+                ingredient.addLink(this.getUriForSelfIngredient(uriInfo,ingredient,sandwich), "self");
+            }
+            sandwich.setIngredientsList(ingredientsList);
+        });
+
+        GenericEntity<List<Sandwich>> listGenericEntity = new GenericEntity<List<Sandwich>>(list) {};
+
+        return Response.ok(listGenericEntity, MediaType.APPLICATION_JSON).build();
     }
 
     @GET
     @Path("/id/{sandwichId}")
-    public Response getSandwich(@PathParam("sandwichId") String sandwichId) {
+    public Response getSandwich(@Context UriInfo uriInfo, @PathParam("sandwichId") String sandwichId) {
         Sandwich sandwich = sandwichResource.findById(sandwichId);
-        if (sandwich != null)
-            return Response.ok(sandwich, MediaType.APPLICATION_JSON).build();
-        else
+
+        if (sandwich == null)
             return Response.status(Response.Status.NOT_FOUND).build();
+
+        List<Ingredient> ingredientsList = sandwich.getIngredientsList();
+
+        sandwich.setIngredientsList(ingredientsList);
+
+        return Response.ok(sandwich, MediaType.APPLICATION_JSON).build();
     }
-    
-    
+
     @POST
     @Path("/add") 
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     public Response add (
-            @FormParam("salade") String salade,
-            @FormParam("crudite") String crudite,
-            @FormParam("charcuterie") String charcuterie,
-            @FormParam("fromage") String fromage,
-            @FormParam("sauce") String sauce,
-            @FormParam("viande") String viande
+            @FormParam("meat") String meat,
+            @FormParam("cold_meats") String coldMeats,
+            @FormParam("cheese") String cheese,
+            @FormParam("salad") String salad,
+            @FormParam("sauce") String sauce
     ) {
 
-        if (sandwichResource.insert(salade,crudite,charcuterie,fromage,sauce,viande) == null)
+        if (sandwichResource.insert(meat, coldMeats, cheese, salad, sauce) == null)
             return Response.status(Response.Status.EXPECTATION_FAILED).build();
 
        return Response.ok().build();
     }
 
+    @DELETE
+    @Path("/id/{sandwichId}")
+    public Response deleteIngredient(@PathParam("sandwichId") String sandwichId) {
+        if (sandwichResource.delete(sandwichId))
+            return Response.ok().build();
+        else
+            return Response.status(Response.Status.NOT_FOUND).build();
+    }
 
+    private String getUriForSelfSandwich(UriInfo uriInfo, Sandwich sandwich) {
+        return uriInfo.getBaseUriBuilder()
+                .path(SandwichRepresentation.class)
+                .path("id/" + sandwich.getId())
+                .build()
+                .toString();
+    }
+
+    private String getUriForSandwich(UriInfo uriInfo) {
+        return uriInfo.getBaseUriBuilder()
+                .path(SandwichRepresentation.class)
+                .build()
+                .toString();
+    }
+
+    private String getUriForSelfIngredient(UriInfo uriInfo, Ingredient ingredient, Sandwich sandwich) {
+        return uriInfo.getBaseUriBuilder()
+                .path(IngredientRepresentation.class)
+                .path("id/" + ingredient.getId())
+                .build()
+                .toString();
+    }
+
+    private String getUriForIngredient(UriInfo uriInfo, Sandwich sandwich) {
+        return uriInfo.getBaseUriBuilder()
+                .path(IngredientRepresentation.class)
+                .build()
+                .toString();
+    }
 }
