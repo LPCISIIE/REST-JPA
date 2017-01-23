b9e6237adce215d01607927f51bf6fe20ac04ccb
xchopin <xchopin54@gmail.com>
2017-01-23 22:28:25 +0100

    Fixed path

--- src/main/java/boundary/Account/AccountRepresentation.java
+++ src/main/java/boundary/Account/AccountRepresentation.java
@@ -13,7 +13,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-@Path("/account")
+@Path("/accounts")
 @Stateless
 @Produces(MediaType.APPLICATION_JSON)
 public class AccountRepresentation {
