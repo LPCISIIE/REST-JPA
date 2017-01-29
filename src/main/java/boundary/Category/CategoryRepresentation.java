a953bd28dc74a84fa6a3c151a96b0f7fd7f7ce0e
Xavier Chopin <xchopin54@gmail.com>
2017-01-30 00:58:04 +0100

    Translated + Fixed REST + Added links

--- src/main/java/boundary/Category/CategoryRepresentation.java
+++ src/main/java/boundary/Category/CategoryRepresentation.java
@@ -1,26 +1,28 @@
 package boundary.Category;
 
+import boundary.Ingredient.IngredientRepresentation;
 import boundary.Ingredient.IngredientResource;
+import boundary.Sandwich.SandwichResource;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.annotations.ApiResponse;
 import com.wordnik.swagger.annotations.ApiResponses;
+import control.DatabaseSeeder;
 import entity.Category;
 import entity.Ingredient;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.*;
-import javax.ws.rs.core.GenericEntity;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Path("/categories")
 
 @Produces(MediaType.APPLICATION_JSON)
 @Stateless
-@Api(value = "/categories", description = "Gestion des catégories d'aliments")
+@Api(value = "/categories", description = "Ingredient categories management")
 public class CategoryRepresentation {
 
     @EJB
@@ -29,21 +31,23 @@ public class CategoryRepresentation {
     @EJB
     CategoryResource categoryResource;
 
+    @EJB
+    SandwichResource sandwichResource;
+
     @GET
-    @ApiOperation(value = "Récupération de toutes les catégories existantes",
-         notes = "Accès: Client, Admin")
+    @ApiOperation(value = "Get all the categories", notes = "Access : Guest, Customer and Admin")
     @ApiResponses(value = {
         @ApiResponse(code = 200, message = "OK"),
         @ApiResponse(code = 500, message = "Internal server error")})
     public Response getCategories() {
+        DatabaseSeeder.feedCatalog(ingredientResource,categoryResource,sandwichResource);
         GenericEntity<List<Category>> list = new GenericEntity<List<Category>>(categoryResource.findAll()){};
         return Response.ok(list, MediaType.APPLICATION_JSON).build();
     }
 
     @GET
-    @Path("/id/{id}")
-    @ApiOperation(value = "Récupération d'une catégorie par son id",
-         notes = "Accès: Client, Admin")
+    @Path("/{id}")
+    @ApiOperation(value = "Get a category by its id", notes = "Access : Guest, Customer and Admin")
     @ApiResponses(value = {
         @ApiResponse(code = 200, message = "OK"),
         @ApiResponse(code = 404, message = "Not Found"),
@@ -58,42 +62,58 @@ public class CategoryRepresentation {
 
     @GET
     @Path("/name/{name}")
-    @ApiOperation(value = "Récupération d'une catégorie par son nom",
-         notes = "Accès: Client, Admin")
+    @ApiOperation(value = "Get categories by their name", notes = "Access : Guest, Customer and Admin - Can be one or many categories")
     @ApiResponses(value = {
         @ApiResponse(code = 200, message = "OK"),
         @ApiResponse(code = 404, message = "Not Found"),
         @ApiResponse(code = 500, message = "Internal server error")})
     public Response getCategoryByName(@PathParam("name") String categoryName) {
+        DatabaseSeeder.feedCatalog(ingredientResource,categoryResource,sandwichResource);
+
         List<Category> categories = categoryResource.findByName(categoryName);
 
         if (categories.isEmpty())
             return Response.status(Response.Status.NOT_FOUND).build();
-        
+
         GenericEntity<List<Category>> list = new GenericEntity<List<Category>>(categories){};
         return Response.ok(list, MediaType.APPLICATION_JSON).build();
     }
 
     @GET
-    @Path("/ingredients/{id}")
-    @ApiOperation(value = "Récupération des ingrédients d'une catégorie",
-         notes = "Accès: Client, Admin")
+    @Path("/{id}/ingredients")
+    @ApiOperation(value = "Get the ingredients of a category", notes = "Access : Guest, Customer and Admin")
     @ApiResponses(value = {
         @ApiResponse(code = 200, message = "OK"),
         @ApiResponse(code = 404, message = "Not Found"),
         @ApiResponse(code = 500, message = "Internal server error")})
-    public Response getCategoryIngredients(@PathParam("id") String categoryId) {
+    public Response getCategoryIngredients(@Context UriInfo uriInfo, @PathParam("id") String categoryId) {
+
+        if (categoryId == null)
+            return Response.status(Response.Status.NOT_FOUND).build();
+
         Category cat = categoryResource.findById(categoryId);
-        
+
         if (cat == null)
             return Response.status(Response.Status.NOT_FOUND).build();
-        
-        List<Ingredient> ingredients = ingredientResource.findByCategory(cat.getId());
+
+        List<Ingredient> ingredients = ingredientResource.findByCategory(cat);
         if(ingredients.isEmpty())
             return Response.status(Response.Status.NOT_FOUND).build();
-            
+
+        for (Ingredient ingredient : ingredients) {
+           ingredient.getLinks().clear();
+           ingredient.addLink(getUriForSelfIngredient(uriInfo, ingredient), "self");
+        }
+
         GenericEntity<List<Ingredient>> list = new GenericEntity<List<Ingredient>>(ingredients){};
         return Response.ok(list, MediaType.APPLICATION_JSON).build();
     }
 
+    private String getUriForSelfIngredient(UriInfo uriInfo, Ingredient ingredient) {
+        return uriInfo.getBaseUriBuilder()
+                .path(IngredientRepresentation.class)
+                .path("id/" + ingredient.getId())
+                .build()
+                .toString();
+    }
 }
