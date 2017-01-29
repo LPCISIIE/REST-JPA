f95f6014ce21476372ef30b06ce7308a18c398d4
Xavier Chopin <xchopin54@gmail.com>
2017-01-29 23:43:22 +0100

    Changed name

--- src/main/java/com/airhacks/ApplicationConfig.java
+++ src/main/java/com/airhacks/ApplicationConfig.java
@@ -4,7 +4,7 @@ package com.airhacks;
 import java.util.Set;
 import javax.ws.rs.core.Application;
 
-@javax.ws.rs.ApplicationPath("rest")
+@javax.ws.rs.ApplicationPath("docs")
 public class ApplicationConfig extends Application {
 
     @Override
