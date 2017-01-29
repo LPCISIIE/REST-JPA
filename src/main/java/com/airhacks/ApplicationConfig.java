54f722435b6af079ba8dad1e8463079bde71573d
iryu54 <corentinlabroche@gmail.com>
2017-01-29 00:06:39 +0000

    Ajout de la documentation pour le back office + oublis de la securisation sur le signup

--- src/main/java/com/airhacks/ApplicationConfig.java
+++ src/main/java/com/airhacks/ApplicationConfig.java
@@ -27,6 +27,7 @@ public class ApplicationConfig extends Application {
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(boundary.Account.AccountRepresentation.class);
         resources.add(boundary.Account.AuthenticationEndpoint.class);
+        resources.add(boundary.BackOffice.BackOfficeRepresentation.class);
     }
 
 }
